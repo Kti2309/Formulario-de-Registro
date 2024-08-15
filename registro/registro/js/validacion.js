484274671c1f37113d63af61baa81e760054d167
Kti2309 <Cotinegreyra104@gmail.com>
2024-08-15 09:41:15 -0300

    validación de contraseñas

--- registro/registro/js/validacion.js
+++ registro/registro/js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function validarContraseña() {
+    if (document.getElementsByClassName("password1").length<6){
+        showAlertError()
+    }else{
+        return false;
+    }
+
+}
+
+function validarRepetirContraseña() {
+    if (document.getElementsById("password1") === document.getElementsByClassName("password2")){
+
+        return true;  
+    } else{ showAlertError()
+
+    }
+     
+}
